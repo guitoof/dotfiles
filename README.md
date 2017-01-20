76ce0fc7cad00627b030c076118a4b854e077968
Guillaume Diallo-Mulliez <guillaumedm@bamlab.fr>
2017-01-20 01:02:38 +0100

    Add sync command

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+Synchronize your machine with the dotfiles contained in this repo:
+```
+./sync.sh
+```
+
+:warning: WARNING
+Files in your home `~` will be overrided
---
+++ sync.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+EXCLUDED_ITEMS=(
+  ".DS_Store" ".git/" "sync.sh" "README.md"
+)
+
+git pull origin master;
+
+function build_options() {
+  OPTIONS=''
+  for ITEM in ${EXCLUDED_ITEMS[@]}
+  do
+    OPTIONS="$OPTIONS --exclude $ITEM"
+  done
+  echo $OPTIONS
+}
+
+function sync() {
+  OPTIONS=$(build_options);
+  rsync $OPTIONS -avh --no-perms . ~;
+#	ls -A | grep -e ^\\. | while read file; do
+#		cat $file >> ~/${file};
+#    done;
+  source ~/.zshrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  sync;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		sync;
+	fi;
+fi;
+unset sync;
